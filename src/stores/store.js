9a08585a594ec6d2a16402d97068ecd9c47ecbd2
Katie <katlizrao@gmail.com>
2025-04-13 19:04:23 -0500

    FIXES

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,21 +1,46 @@
-import { defineStore } from 'pinia'
-import { markRaw, ref } from 'vue'
-import AppIntro from '@/components/AppIntro.vue'
+import { defineStore } from "pinia";
+import { markRaw, ref } from "vue";
+import AppIntro from "@/components/AppIntro.vue";
 
-export const useStore = defineStore('store', () => {
-  const currentView = ref(markRaw(AppIntro)) // Mark as raw so Vue doesn't reactively track the component
+export const useStore = defineStore("store", () => {
+  const currentView = ref(markRaw(AppIntro)); // Mark as raw so Vue doesn't reactively track
 
-  const zoomRequested = ref(false)
+  const zoomIn = ref(false);
+  const zoomOut = ref(false);
+  const zoomReset = ref(false);
 
   function setView(viewName) {
-    currentView.value = markRaw(viewName) // Mark as raw when updating
+    currentView.value = markRaw(viewName); // Mark as raw when updating
   }
-
-  function RequestZoom() {
-    zoomRequested.value = true
+  function RequestZoomIn() {
+    // console.log("Zoom in request received");
+    zoomIn.value = true;
+  }
+  function RequestZoomOut() {
+    // console.log("Zoom out request received");
+    zoomIn.value = false;
+    zoomOut.value = true;
+  }
+  function RequestZoomStop() {
+    // console.log("Zoom stop request received");
+    zoomIn.value = false;
+    zoomOut.value = false;
+    zoomReset.value = false;
+  }
+  function RequestZoomReset() {
+    // console.log("Zoom reset request received");
+    zoomReset.value = true;
   }
 
   return {
     currentView,
-  }
-})
+    setView,
+    zoomIn,
+    zoomOut,
+    zoomReset,
+    RequestZoomIn,
+    RequestZoomOut,
+    RequestZoomStop,
+    RequestZoomReset,
+  };
+});
